daf6216b9b871a037d3a80d0834e8d20ad16cc2f
Joselu <joseluissgz97@gmail.com>
2023-12-26 17:43:45 +0100

    minimap-component

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  templateUrl: './mini-map.component.html',
+  styleUrls: ['./mini-map.component.scss']
+})
+export class MiniMapComponent {
+
+}
