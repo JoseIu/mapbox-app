81ff3669e043554c79ce302cfc32fd4aa43cc7aa
Joselu <joseluissgz97@gmail.com>
2023-12-26 17:44:29 +0100

    layout for maps

---
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps-layout',
+  templateUrl: './maps-layout.component.html',
+  styleUrls: ['./maps-layout.component.scss']
+})
+export class MapsLayoutComponent {
+
+}
