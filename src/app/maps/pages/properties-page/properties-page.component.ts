3772aa10626ae2fa0a1cf4399b4717be6d8bb3fe
Joselu <joseluissgz97@gmail.com>
2023-12-26 17:45:23 +0100

    properties page

---
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-properties-page',
+  templateUrl: './properties-page.component.html',
+  styleUrls: ['./properties-page.component.scss']
+})
+export class PropertiesPageComponent {
+
+}
