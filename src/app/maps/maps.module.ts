e8f3304090ae1b04aa986d507748b9a3b2616255
Joselu <joseluissgz97@gmail.com>
2023-12-26 17:46:42 +0100

    maps module, importing mabox.accessToken

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+import { enviroment } from 'src/environments/environments';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
+(mapboxgl as any).accessToken = enviroment.mapbox_key;
+
+@NgModule({
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomPageComponent,
+  ],
+  imports: [CommonModule, MapsRoutingModule],
+})
+export class MapsModule {}
